01ca195f6020a7cc1e1f48d766c0ca1ea3893d84
Tufan Özen <tufan.oezen@protonmail.com>
2026-05-04 01:01:15 +0200

    hya-69/feat: Event/s

---
+++ crates/ecs/src/event/tests/event_trait_test.rs
@@ -0,0 +1,22 @@
+use crate::event::Event;
+
+struct TestComponent;
+struct TestComponent2;
+impl Event for TestComponent {}
+impl Event for TestComponent2 {}
+
+#[test]
+fn test_component_same_type() {
+    assert_eq!(
+        std::any::TypeId::of::<TestComponent>(),
+        std::any::TypeId::of::<TestComponent>()
+    );
+}
+
+#[test]
+fn test_component_different_type() {
+    assert_ne!(
+        std::any::TypeId::of::<TestComponent>(),
+        std::any::TypeId::of::<TestComponent2>()
+    );
+}
