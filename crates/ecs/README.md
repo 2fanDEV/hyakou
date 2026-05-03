86d14d8c76561ece78a79d3abac82e9e29861d52
Tufan Özen <tufan.oezen@protonmail.com>
2026-05-03 23:24:45 +0200

    HYA-65/66/feat: module layout & mise test command & inclusion into ci tests

---
+++ crates/ecs/README.md
@@ -0,0 +1,6 @@
+## Test
+
+Run tests via:
+```rust
+cargo test --all-targets --all-features -- --nocaptur
+```
--- crates/ecs/src/lib.rs
+++ crates/ecs/src/lib.rs
@@ -1,1 +1,9 @@
-
+pub mod commands;
+pub mod component;
+pub mod entity;
+pub mod event;
+pub mod query;
+pub mod resource;
+pub mod schedule;
+mod tests;
+pub mod world;
