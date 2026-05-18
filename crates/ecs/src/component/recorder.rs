299edba307780aefd80338cea27662dc3170073a
Tufan Özen <tufan.oezen@protonmail.com>
2026-05-18 23:36:49 +0200

    HYA-87: alive-gated component command recording

--- crates/ecs/src/component/recorder.rs
+++ crates/ecs/src/component/recorder.rs
@@ -8,7 +8,7 @@ pub struct ComponentRecorder<'a> {
 impl<'a> ComponentRecorder<'a> {
     pub fn insert<C: Component>(&mut self, entity: &mut EntityId, component: C) {
         let allocator = self.components.allocator();
-        if let Ok(_) = allocator.try_read_shared(|alloc| alloc.is_alive(entity)) {
+        if let Ok(true) = allocator.try_read_shared(|alloc| alloc.is_alive(entity)) {
             self.components
                 .storage_mut::<C>()
                 .insert_command(ComponentCommand::Insert {
@@ -20,7 +20,7 @@ impl<'a> ComponentRecorder<'a> {
 
     pub fn remove<C: Component>(&mut self, entity: &mut EntityId) {
         let allocator = self.components.allocator();
-        if let Ok(_) = allocator.try_read_shared(|alloc| alloc.is_alive(entity)) {
+        if let Ok(true) = allocator.try_read_shared(|alloc| alloc.is_alive(entity)) {
             {
                 self.components
                     .storage_mut::<C>()
